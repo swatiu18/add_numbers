e411b4c8b9986aaf0795267cf9df0e7b52eb08c9
swatiu18 <85886325+swatiu18@users.noreply.github.com>
2021-09-06 15:32:32 +0530

    Add files via upload

---
+++ swati_add.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter two numbers : ";
+    cin>>a>>b;
+    cout<<"sum of these numbers : ";
+    cout<<a+b;
+
+}
